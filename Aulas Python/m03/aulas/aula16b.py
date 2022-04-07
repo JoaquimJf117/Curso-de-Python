2ac9b53be5a010e06adbb874c1e8e727b5a30f59
Fernandes Joaquim <64234688+joaquimjfernandes@users.noreply.github.com>
2022-04-08 00:13:01 +0100

    apagando

--- Aulas Python/m03/aulas/aula16b.py
+++
@@ -1,4 +0,0 @@
-print('='*15, '\033[1;35mAULA 16 - Tuplas[Testes]\033[m', '='*15)
-# Tuplas são IMUTÁVEIS
-lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
-print(sorted(lanche))  # Sorted ordena os elementos dentro da Tupla
--- Aulas Python/m03/aulas/aula18b.py
+++
@@ -1,4 +0,0 @@
-print('=' * 15, '\033[1;35mAULA 18 - Listas[Testes]\033[m', '=' * 15)
-galera = [['João', 13], ['Ana', 33], ['Joaquim', 19], ['Maria', 45]]
-for p in galera:
-    print(f'{p[0]} tem {p[1]} Anos de Idades.')
